90e9db9229e21e6ca6d418281b6235f7d64ef845
marco decarlo <marco.decarlo@dechit.it>
2021-10-02 20:05:37 +0200

    add modal product

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,26 +1,89 @@
-const Cards = ({product}) => {
+import { useState } from "react";
+
+const Cards = ({ product }) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="group relative">
-      <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-        <img
-          src={product.image}
-          alt="image"
-          className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-        />
-      </div>
-      <div className="mt-4 flex justify-between">
-        <div>
-          <h3 className="text-sm text-gray-700">
-            <a href="#">
-              <span aria-hidden="true" className="absolute inset-0"></span>
-              {product.title}
-            </a>
-          </h3>
-          <p className="mt-1 text-sm text-gray-500">{product.category}</p>
+    <>
+      <div className="group relative" onClick={() => setShowModal(true)}>
+        <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img
+            src={product.image}
+            alt="image"
+            className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+          />
         </div>
-        <p className="text-sm font-medium text-gray-900 whitespace-nowrap">{product.price} €</p>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm text-gray-700">
+                {product.title}
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">{product.category}</p>
+          </div>
+          <p className="text-sm font-medium text-gray-900 whitespace-nowrap">
+            {product.price} €
+          </p>
+        </div>
       </div>
-    </div>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-2xl font-semibold">{product.title}</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-3 flex-auto">
+                  <div class="grid grid-cols-2 gap-4">
+                    <div className="w-full p-3">
+                      <img
+                        src={product.image}
+                        alt="image"
+                        className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                      />
+                    </div>
+                    <div className="w-full p-3">
+                      <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                        {product.description}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-green-500 text-white active:bg-green-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
   );
 };
 
