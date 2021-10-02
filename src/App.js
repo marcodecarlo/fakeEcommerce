291cb3580587542afed498f6ac2b3d5e3714473e
marco decarlo <marco.decarlo@dechit.it>
2021-10-02 16:56:27 +0200

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import './App.css';
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
