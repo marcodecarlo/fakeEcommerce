002d0006071e3f0d49bff4fb84f05817d2d5dbfd
marco decarlo <marco.decarlo@dechit.it>
2021-10-03 09:30:21 +0200

    add spinner

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,27 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import Cards from "../components/Cards";
+import Spinner from "../components/Spinner";
 
 import * as productActions from "../redux/actions/productActions";
 
-const HomePage = ({ product, loadProduct }) => {
+const HomePage = ({ loading, product, loadProduct }) => {
+  const [error, setError] = useState();
+
   useEffect(() => {
     loadProduct().catch((error) => {
-      alert("Loading product failed" + error);
+      setError(error);
     });
   }, []);
 
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 ">
-      <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
-        I nostri prodotti
-      </h2>
- 
-      <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {product && product.map((ele, index) => <Cards product={ele} />)}
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          {error ? (
+            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 ">
+                <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+                    Error
+                </h2>
+            </div>
+          ) : (
+            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 ">
+              <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+                I nostri prodotti
+              </h2>
+
+              <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                {product &&
+                  product.map((ele, index) => <Cards product={ele} />)}
+              </div>
+            </div>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
