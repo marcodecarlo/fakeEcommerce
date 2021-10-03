228f3de21203f4a01605982942030c2ca134915a
marco decarlo <marco.decarlo@dechit.it>
2021-10-04 00:16:28 +0200

    edit layout & responsive - shopping cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
 import * as productActions from "../redux/actions/productActions";
-
 
 const ShoppingCart = ({ products, cart, readProduct }) => {
   const [show, setShow] = useState(false);
   const [productCart, setProductCart] = useState([]);
 
   useEffect(() => {
-   
     if (Object.keys(cart.products).length > 0) {
       cart.products.forEach(function (element) {
-        let productList = products.inCart.filter((item) => item.id == element.productId);
+        let productList = products.inCart.filter(
+          (item) => item.id == element.productId
+        );
         if (productList.length <= 0) {
           setProductCart([...productCart, element]);
         }
@@ -41,58 +43,36 @@ const ShoppingCart = ({ products, cart, readProduct }) => {
   return (
     <div className="absolute top-20 right-12 z-50">
       <button
-        className="bg-red-600 text-white rounded-full h-14 w-14 flex items-center justify-center fixed top-6 right-12"
+        className="bg-trasparent text-green-600 rounded-full h-10 w-10 flex items-center justify-center fixed top-6 right-12 border-green-600 border"
         type="button"
         onClick={() => {
           show ? closeShoppingCart() : openShoppingCart();
         }}
       >
-        Carrello
+          {show ?
+        <FontAwesomeIcon icon={faTimes} />
+            :
+        <FontAwesomeIcon icon={faShoppingCart} />
+          }
       </button>
       {show ? (
-        <div className="w-screen max-w-md">
+
+        <div className="w-screen max-w-md fixed top-16 right-0 h-full md:top-16 md:right-12 z-50 cart">
           <div className="h-full flex flex-col bg-white shadow-xl">
             <div className="flex-1 py-6 overflow-y-auto px-4 sm:px-6">
               <div className="flex items-start justify-between">
                 <h2
-                  className="text-lg font-medium text-gray-900"
+                  className="text-lg font-medium text-gray-900 text-green-600 font-sans uppercase"
                   id="slide-over-title"
                 >
                   Shopping cart
                 </h2>
-                <div className="ml-3 h-7 flex items-center">
-                  <button
-                    type="button"
-                    className="-m-2 p-2 text-gray-400 hover:text-gray-500"
-                  >
-                    <span className="sr-only">Close panel</span>
-
-                    <svg
-                      className="h-6 w-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
-                </div>
               </div>
 
-              <div className="mt-4">
-                <div className="flow-root">
-                  <ul
-                    role="list"
-                    className="my-1 divide-y divide-gray-200"
-                  >
-                      {products.inCart.length > 0 &&
+              <div className="flow-root">
+                {products.inCart.length > 0 ? (
+                  <ul role="list" className="my-1 divide-y divide-gray-200">
+                    {products.inCart.length > 0 &&
                       products.inCart.map((ele, index) => (
                         <li className="py-6 flex">
                           <div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
@@ -112,43 +92,42 @@ const ShoppingCart = ({ products, cart, readProduct }) => {
                                 <p className="ml-4">{ele.price}</p>
                               </div>
                             </div>
-                            <div className="flex-1 flex items-end justify-between text-sm">
-                             
-
-                              <div className="flex">
-                                <button
-                                  type="button"
-                                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                                >
-                                  Remove
-                                </button>
-                              </div>
-                            </div>
                           </div>
                         </li>
                       ))}
                   </ul>
-                </div>
+                ) : (
+                  <div className="w-full">
+                    <p>is empty</p>
+                  </div>
+                )}
               </div>
             </div>
 
-            <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
-              <div className="flex justify-between text-base font-medium text-gray-900">
-                <p>Subtotal</p>
-                <p>$262.00</p>
+            {products.inCart.length > 0 && (
+              <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+                <div className="flex justify-between text-base font-medium text-gray-900">
+                  <p>Subtotal</p>
+                  <p>
+                    {parseFloat(products.inCart.reduce(function (a, b) {
+                      debugger;
+                      return a + b["price"];
+                    }, 0)).toFixed(2)} €
+                  </p>
+                </div>
+                <p className="mt-0.5 text-sm text-gray-500">
+                  Shipping and taxes calculated at checkout.
+                </p>
+                <div className="mt-6">
+                  <a
+                    href="#"
+                    className="flex justify-center items-center px-6 py-3 btn-primary"
+                  >
+                    Checkout
+                  </a>
+                </div>
               </div>
-              <p className="mt-0.5 text-sm text-gray-500">
-                Shipping and taxes calculated at checkout.
-              </p>
-              <div className="mt-6">
-                <a
-                  href="#"
-                  className="flex justify-center items-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Checkout
-                </a>
-              </div>
-            </div>
+            )}
           </div>
         </div>
       ) : null}
@@ -165,7 +144,7 @@ function mapStateToProps(state) {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    readProduct: bindActionCreators(productActions.readProduct, dispatch),
-  });
+  readProduct: bindActionCreators(productActions.readProduct, dispatch),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCart);
